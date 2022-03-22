67bb7ce673efeb62b58a8a3c58bcc5927926e155
Vinicius Jimenez <vinnicius.jimenez@gmail.com>
2022-03-22 15:34:23 -0300

    finished: dashboard screen

---
+++ src/views/dashboard.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@500;600;700&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="../../public/styles/global.css">
+
+    <link rel="stylesheet" href="../../public/styles/dashboard.css">
+
+    <title>Clantina | Dashboard</title>
+</head>
+<body>
+    <main class="container">
+        <header class="dashboard-header">
+            <h1 class="dashboard-header__title"><span class="dashboard-header__title--yellow">Clan</span>tina</h1>
+        </header>
+
+        <section class="dashboard-sale-container">
+            <a href="">
+                <strong>Nova venda <img src="../../public/images/plus.svg" alt="Seta para a direita"></strong>
+            </a>
+        </section>
+
+        <nav class="dashboard-menu">
+            <ul>
+                <li class="dashboard-menu__item">
+                    <a href="">Vendas <span class="dashboard-menu__item--chevron"><img src="../../public/images/right-chevron.svg" alt="Seta apontando para direits"></span></a>
+                </li>
+                <li class="dashboard-menu__item">
+                    <a href="">Estoque <span class="dashboard-menu__item--chevron"><img src="../../public/images/right-chevron.svg" alt="Seta apontando para direits"></span></a>
+                </li>
+            </ul>
+        </nav>
+    </main>
+</body>
+</html>

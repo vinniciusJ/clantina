c29c66e4d43f1336aab790531e5bbaaaa94a07bb
DyogoBendo <dyogoromagnabendo@gmail.com>
2022-03-23 22:37:58 -0300

    starting dal

---
+++ src/dal/connection.php
@@ -0,0 +1,21 @@
+<?php
+    include_once('database_info.php');
+	$host = $_ENV["dbhost"];
+	$user = $_ENV["user"];
+	$password = $_ENV["password"];
+	$dbname = $_ENV["db"];
+		
+	$con = mysqli_connect($host, $user, $password, $dbname);
+    class Connection
+    {
+        static function conectar()
+        {
+            try {
+            $pdo = new PDO('mysql:host={$host};dbname={$dbname}', $user, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+            } catch (PDOException $e) {
+                echo 'Error: ' . $e->getMessage();
+            }
+        }
+    }

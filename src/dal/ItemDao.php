8386fd2dcd1db4bbd8dff211170fc3429cb1be47
DyogoBendo <dyogoromagnabendo@gmail.com>
2022-03-24 19:45:42 -0300

    creating Item Dao

--- src/dal/ItemDao.php
+++ src/dal/ItemDao.php
@@ -9,5 +9,20 @@
         function insertUnsaledItem(Item $item){
             $sql = "insert into item(id_item_type, status, purchase_price, price) 
             values ('{$item->getIdItemType()}', '{$item->getStatus()}', '{$item->getPurchasePrice()}', '{$item->getPrice()}'";
+            $this->conn->exec($sql);
+        }
+
+        function listAllItems(){
+            $sql = "select * from item";
+            $query = $this->conn->query($sql);
+            $dados = $query->fetchAll(PDO::FETCH_OBJ);
+            return $dados;
+        }
+
+        function listAllActiveItems(){
+            $sql = "select * from item where status = 'active'";
+            $query = $this->conn->query($sql);
+            $dados = $query->fetchAll(PDO::FETCH_OBJ);
+            return $dados;
         }
     }

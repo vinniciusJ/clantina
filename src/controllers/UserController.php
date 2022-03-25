3c2683fdcbdfeeffce1d251ecd6400153b836934
DyogoBendo <dyogoromagnabendo@gmail.com>
2022-03-25 15:29:05 -0300

    login working

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -1,6 +1,10 @@
 <?php
-    require_once __DIR__ . "../dal/UserDao.php";
-    require_once __DIR__ . "../models/User.php";
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    require_once __DIR__ . "/../dal/UserDao.php";
+    require_once __DIR__ . "/../models/User.php";
 
     class UserController{
         private $dao;        
@@ -13,14 +17,32 @@
             $user = new User($_POST["name"], $_POST["username"], $_POST["password"], $_POST["type"]);            
             $this->dao->insertUser($user);
         }
+
+        function login(){                        
+            $result = $this->dao->checkIfMatchPassword($_POST["username"], $_POST["password"]);
+
+            if(sizeof($result) == 0){
+                header("Location:../views/login.php");          
+            } else{
+                session_start();
+                $user = $result[0];
+                $_SESSION['username'] = $user->username;
+                $_SESSION['name'] = $user->name;
+                $_SESSION['type'] = $user->type;
+                
+                header("Location:../views/dashboard.php");          
+            }
+        }
     }
 
     $userController = new UserController();
 
     $action = $_REQUEST["action"];
-
+    
     switch($action){
         case 1:
             $userController->registerUser();
+        case 2:
+            $userController->login();
     }
     
